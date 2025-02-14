7ff0fb0a1dc95632c3cc5474896185a0e7c1e6d3
Aman Menda <aman.menda@epitech.eu>
2025-02-14 12:48:54 +0100

    remove uvicorn run

--- app/main.py
+++ app/main.py
@@ -22,7 +22,3 @@ fake_menu = [
 @app.get("/")
 async def root():
     return {"message": "Bienvenue sur l'API du Food Truck!"}
-
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
